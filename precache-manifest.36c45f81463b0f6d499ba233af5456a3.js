99831e1035984eb365fcfc78156570b1d7646996
rmarkam <mrmarkam@gmail.com>
2020-04-18 08:46:47 +0530

    Updates

---
+++ precache-manifest.36c45f81463b0f6d499ba233af5456a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92deee336cbf86614ac16cd5986a5532",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0b5395af29fd6c740cd6",
+    "url": "/monsters-rolodex/static/css/main.4e105d62.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b5395af29fd6c740cd6",
+    "url": "/monsters-rolodex/static/js/main.c19ffdfc.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
